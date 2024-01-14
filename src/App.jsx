d9140881d3e020009d894da6ad7b09bac006c287
Kaj Białas <kaj.bialas@dev-industry.com>
2024-01-14 10:27:43 +0100

    Server

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-//Zadanie 1 - useEffect + useState
-// Utwórz komponent pobierający i wyświetlający liste postów z Twojego lokalnego API
-// Zadbaj o obsługę błędów.
+//Zadanie 2 - useEffect + useState + formularze
+// Utwórz komponent Add post, który będzie zawierał formularz dodawania nowego posta
+// Post powinien zostać dodany w pliku db.json
+// Zadbaj o obsługę błędów
 
 import PostList from "./components/PostList";
 
 function App() {
+
+  const addNewPost = async () => {
+    const response = await fetch('http://localhost:3500/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id: 4, title: 'Nowy jakis post'}),
+    })
+  };
+
+
   return (
     <div>
+      <button onClick={() => addNewPost()}>Dodaj posta</button>
       <PostList />
     </div>
   )
